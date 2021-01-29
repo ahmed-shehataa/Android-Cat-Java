3727cf007e9fd587767dabdd8e6a07c33d763554
Rana Ashraf <55793250+RanaAshraf2000@users.noreply.github.com>
2021-01-29 22:17:22 +0200

    Update test.java

--- Task 2/test.java
+++ Task 2/test.java
@@ -1,3 +1,32 @@
-/*
-Try to upload your java file here like this file. 
-*/
+package pkgtry.pkgcatch;
+
+import java.util.Scanner;
+
+
+public class TryCatch {
+
+   
+    public static void main(String[] args) {
+        try{
+        int a[]=new int[6];
+
+          Scanner scan =new Scanner(System.in);
+          for(int i=0; i<=a.length; i++)
+            {
+                System.out.println("enter the number:");  
+                a[i]= scan.nextInt();
+                
+            }//end_for
+          
+            for(int i=0; i<=a.length; i++)
+            {
+                System.out.println(a[i]);
+            }//end_for
+
+        }//end try
+        catch(Exception e){
+            System.out.println("index larger than size of array");
+        }//end catch
+    }//end main
+    
+}//end TryCatch
